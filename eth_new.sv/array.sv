173bbea004f4ef92993caaec6f62b2a2cd61a566
nilakantha <nilakantha@nilakantha-HP-ProBook-4440s.(none)>
2014-07-17 07:04:51 +0530

    packed and unpacked array

---
+++ eth_new.sv/array.sv
@@ -0,0 +1,11 @@
+module packed_unpacked_data();
+bit[7:0]sfd=8'hAB;
+reg sfd1[7:0]='{1,0,1,0,1,0,1,1};
+initial begin
+$display("example of packed and unpacked array\n");
+$display("sfd[0]=%b",sfd[0]);
+$display("sfd1[0]=%b",sfd1[0]);
+$display("sfd=%b\n",sfd);
+end
+endmodule
+
